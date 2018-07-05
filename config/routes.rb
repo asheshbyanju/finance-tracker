a5cf9ab298f76980e1f37dd8d90cda8c83b28073
asheshbyanju <byanju.ashesh@gmail.com>
2018-07-05 17:30:57 +0545

    devise configured to add first name and last name for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :conrotters => { :registrations => "user/registrations" }
   resources :user_stocks, expect: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
